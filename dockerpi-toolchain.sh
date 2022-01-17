66882319e29289c7fa4f7781faf6c9bca6d28bfe
Vitaly Petrov <v31337@gmail.com>
2022-01-17 23:33:34 +0300

    Some fixes

--- dockerpi-toolchain.sh
+++ dockerpi-toolchain.sh
@@ -31,6 +31,22 @@ then
     exit 1
 fi
 
+if [ "${TARGET_ARCH}" = "armv6" ]; then
+    TOOLCHAIN_TARGET="arm-linux-gnueabihf"
+else if [ "${TARGET_ARCH}" = "armv7" ]; then
+    TOOLCHAIN_TARGET="arm-linux-gnueabihf"
+else
+    TOOLCHAIN_TARGET="aarch64-linux-gnueabihf"
+fi
+
+if [ "${TARGET_ARCH}" = "armv6" ]; then
+    TOOLCHAIN_ARM="arm"
+else if [ "${TARGET_ARCH}" = "armv7" ]; then
+    TOOLCHAIN_ARM="arm"
+else
+    TOOLCHAIN_ARM="arm64"
+fi
+
 echo "Remove exists $TOOLCHAIN_PATH"
 
 rm -rf $TOOLCHAIN_PATH
@@ -41,7 +57,7 @@ then
     exit 1
 fi
 
-docker build -f Dockerfile.toolchain --build-arg TOOLCHAIN_PATH_ARG=${TOOLCHAIN_PATH} --build-arg TOOLCHAIN_ARCH_ARG=${TARGET_ARCH} --network=host -t dockerpi/toolchain-${TARGET_ARCH} .
+docker build -f Dockerfile.toolchain --build-arg TOOLCHAIN_PATH_ARG=${TOOLCHAIN_PATH} --build-arg TOOLCHAIN_ARCH_ARG=${TARGET_ARCH} --build-arg TOOLCHAIN_TARGET_ARG=${TOOLCHAIN_TARGET} --build-arg TOOLCHAIN_ARM_ARG=${TOOLCHAIN_ARM} --network=host -t dockerpi/toolchain-${TARGET_ARCH} .
 docker run -v /opt:/opt/mount --rm dockerpi/toolchain-${TARGET_ARCH} bash -c "cp -r ${TOOLCHAIN_PATH} /opt/mount/"
 ${TOOLCHAIN_PATH}/bin/arm-linux-gnueabihf-cpp --version
 
