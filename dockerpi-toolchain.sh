e216618ef44c1b0b8ee1375f54c47108e386c0dd
Vitaly Petrov <v31337@gmail.com>
2022-01-19 22:57:30 +0300

    Fix hard float in toolchain

--- dockerpi-toolchain.sh
+++ dockerpi-toolchain.sh
@@ -60,7 +60,7 @@ then
     exit 1
 fi
 
-docker build -f Dockerfile.toolchain --build-arg TOOLCHAIN_PATH_ARG=${TOOLCHAIN_PATH} --build-arg TOOLCHAIN_ARCH_ARG=${TARGET_ARCH} --build-arg TOOLCHAIN_TARGET_ARG=${TOOLCHAIN_TARGET} --build-arg TOOLCHAIN_ARM_ARG=${TOOLCHAIN_ARM} --build-arg TOOLCHAIN_FLOAT_ARG=${TOOLCHAIN_FLOAT} --network=host -t dockerpi/toolchain-${TARGET_ARCH} .
+docker build -f Dockerfile.toolchain --build-arg TOOLCHAIN_PATH_ARG=${TOOLCHAIN_PATH} --build-arg TOOLCHAIN_ARCH_ARG=${TARGET_ARCH} --build-arg TOOLCHAIN_TARGET_ARG=${TOOLCHAIN_TARGET} --build-arg TOOLCHAIN_ARM_ARG=${TOOLCHAIN_ARM} --build-arg TOOLCHAIN_FLOAT_ARG="${TOOLCHAIN_FLOAT}" --network=host -t dockerpi/toolchain-${TARGET_ARCH} .
 docker run -v /opt:/opt/mount --rm dockerpi/toolchain-${TARGET_ARCH} bash -c "cp -r ${TOOLCHAIN_PATH} /opt/mount/"
 ${TOOLCHAIN_PATH}/bin/${TOOLCHAIN_TARGET}-cpp --version
 
